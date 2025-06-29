877d82ecbc55751082c56052dd12989911360225
joeinman <joe.inman8@gmail.co.uk>
2025-06-29 19:30:15 +0100

    Refactor neo_example.cpp to improve readability and structure; use shared pointers for Neo and add scene components.

--- examples/neo/src/neo_example.cpp
+++ examples/neo/src/neo_example.cpp
@@ -3,26 +3,33 @@
 
 #include <rpixel/rpixel.hpp>
 #include <neo/neo.hpp>
+#include <neo/rectangle.hpp>
 
 constexpr uint8_t LEDDataPin = 4;
 constexpr uint8_t LEDCount   = 30;
 constexpr bool    IsRGBW     = false;
+
+using namespace jsi::neo;
 
 int main()
 {
     stdio_init_all();
 
     jsi::RPixel led_strip(LEDDataPin, LEDCount, IsRGBW);
-    jsi::Neo    neo(
-        {LEDCount, 1},
+    auto        neo = std::make_shared<Neo>(
+        ScreenSize(LEDCount, 1),
         [&led_strip](size_t index, uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
             led_strip.setPixel(index, r, g, b, w);
         },
         [&led_strip]() { led_strip.show(); },
         time_us_64);
 
+    auto scene = std::make_shared<Scene>();
+    scene->addComponent<Rectangle>(0, 0, PortName("width"), 1, Color(255, 0, 0));
+    neo->loadScene(scene);
+
     while (true)
     {
-        neo.spin();
+        neo->spin();
     }
 }
