8ffce4c130ab32e93a04adecbf9ce24129206398
joeinman <joe.inman8@gmail.co.uk>
2025-07-03 21:25:56 +0100

    Fix rectangle dimensions in neo_example.cpp and update subproject commit for neo library

--- examples/neo/src/neo_example.cpp
+++ examples/neo/src/neo_example.cpp
@@ -18,14 +18,16 @@ int main()
     jsi::RPixel led_strip(LEDDataPin, LEDCount, IsRGBW);
     auto        neo = std::make_shared<Neo>(
         ScreenSize(LEDCount, 1),
-        [&led_strip](size_t index, uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
-            led_strip.setPixel(index, r, g, b, w);
+        [&led_strip](size_t index, uint8_t r, uint8_t g, uint8_t b, uint8_t /*w*/) {
+            led_strip.setPixel(index, r, g, b);
         },
         [&led_strip]() { led_strip.show(); },
         time_us_64);
 
     auto scene = std::make_shared<Scene>();
-    scene->addComponent<Rectangle>(0, 0, PortName("width"), 1, Color(255, 0, 0));
+    scene->addComponent<Rectangle>(0, 0, LEDCount - 10, 1, Color(255, 0, 0));
+    scene->addComponent<Rectangle>(LEDCount - 10, 0, 10, 1, Color(0, 0, 255));
+
     neo->loadScene(scene);
 
     while (true)
