19f537a7aeb25d9890f5426d178a045bee95c0b6
joeinman <joe.inman8@gmail.co.uk>
2025-07-03 23:58:09 +0100

    Refactor NotificationBarScene in neo_example.cpp to encapsulate scene logic and improve waveform handling

--- examples/neo/src/neo_example.cpp
+++ examples/neo/src/neo_example.cpp
@@ -10,11 +10,32 @@ constexpr uint8_t LEDDataPin = 4;
 constexpr uint8_t LEDCount   = 30;
 constexpr bool    IsRGBW     = false;
 
-constexpr uint64_t IndicatorWidth = 5;
-constexpr uint64_t BarWidth       = 8;
-constexpr uint64_t rangeX         = (LEDCount - 2 * IndicatorWidth) - BarWidth + 1;
-
 using namespace jsi::neo;
+
+class NotificationBarScene : public Scene
+{
+    const uint8_t IndicatorWidth = 5;
+    const uint8_t BarWidth       = 8;
+    const uint8_t rangeX         = static_cast<uint8_t>((LEDCount - 2 * IndicatorWidth) - BarWidth + 1);
+
+public:
+    NotificationBarScene()
+    {
+        addComponent<Rectangle>(0, 0, IndicatorWidth, 1, Color(255, 255, 255));
+        addComponent<Rectangle>(LEDCount - IndicatorWidth, 0, IndicatorWidth, 1, Color(255, 255, 255));
+        addComponent<Rectangle>(0, 0, BarWidth, 1, Color(255, 255, 0));
+        addComponent<WaveformGenerator>(1.0, WaveformType::kTriangle);
+    }
+
+    void tick(uint64_t time_us) override
+    {
+        Scene::tick(time_us);
+
+        auto    waveform_value = getComponent(3)->getOutput<double>("waveform_value").value();
+        uint8_t x              = IndicatorWidth + static_cast<uint8_t>(waveform_value * rangeX);
+        setComponentProperty<uint8_t>(2, "x", x);
+    }
+};
 
 int main()
 {
@@ -29,20 +50,11 @@ int main()
         [&led_strip]() { led_strip.show(); },
         time_us_64);
 
-    auto scene = std::make_shared<Scene>();
-    scene->addComponent<Rectangle>(0, 0, IndicatorWidth, 1, Color(255, 255, 255));
-    scene->addComponent<Rectangle>(LEDCount - IndicatorWidth, 0, IndicatorWidth, 1, Color(255, 255, 255));
-    auto bar_id          = scene->addComponent<Rectangle>(0, 0, BarWidth, 1, Color(255, 255, 0));
-    auto waveform_gen_id = scene->addComponent<WaveformGenerator>(1.0, WaveformType::kTriangle);
+    auto scene = std::make_shared<NotificationBarScene>();
     neo->loadScene(scene);
 
     while (true)
     {
-        uint8_t x = IndicatorWidth +
-                    static_cast<uint8_t>(
-                        scene->getComponent(waveform_gen_id)->getOutput<double>("waveform_value").value() * rangeX);
-        scene->setComponentProperty<uint8_t>(bar_id, "x", x);
-
         neo->spin();
     }
 }
